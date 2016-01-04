299bde79b2dbfe885fe6ff4a36e4621ad247fceb
Fabian Meyer <rookfighter@develop>
2016-01-04 17:52:52 +0000

    building test is now optional

---
+++ cmake-modules/FindCatch.cmake
@@ -0,0 +1,10 @@
+# once done this will define
+# CATCH_FOUND
+# CATCH_INCLUDE_DIR
+
+find_path(CATCH_INCLUDE_DIR NAMES catch.hpp
+    HINTS
+    "${CATCH_ROOT}")
+    
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Catch DEFAULT_MSG CATCH_INCLUDE_DIR)
