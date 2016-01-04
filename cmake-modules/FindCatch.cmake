1cfd707b1bd3e6ea0a8e277426c900f1da38f833
Fabian Meyer <rookfighter@develop>
2016-01-04 18:06:15 +0000

    added .travis.yml

--- cmake-modules/FindCatch.cmake
+++ cmake-modules/FindCatch.cmake
@@ -4,7 +4,7 @@
 
 find_path(CATCH_INCLUDE_DIR NAMES catch.hpp
     HINTS
-    "${CATCH_ROOT}")
+    "${CATCH_ROOT}/include")
     
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Catch DEFAULT_MSG CATCH_INCLUDE_DIR)
