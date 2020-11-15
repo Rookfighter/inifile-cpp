b5b89013f45a03508a13d88cdc72f12ed1f84440
Fabian Meyer <user8324@posteo.net>
2020-11-15 09:37:49 +0100

    Implement encode and save example

--- examples/save_ini_file.cpp
+++ examples/save_ini_file.cpp
@@ -1,4 +1,4 @@
-/* encode_ini_file.cpp
+/* save_ini_file.cpp
  *
  * Author: Fabian Meyer
  * Created On: 14 Nov 2020
@@ -7,8 +7,16 @@
 #include <inicpp.h>
 #include <iostream>
 
-int main()
+int main(int argc, char **argv)
 {
+    if(argc != 2)
+    {
+        std::cerr << "usage: save_ini_file [FILE_PATh]" << std::endl;
+        return 1;
+    }
+
+    std::string path = argv[1];
+
     ini::IniFile inif;
 
     inif["Foo"]["hello"] = "world";
@@ -17,10 +25,9 @@ int main()
     inif["Another"]["char"] = 'q';
     inif["Another"]["bool"] = true;
 
-    std::string content = inif.encode();
+    inif.save(path);
 
-    std::cout << "Encoded ini file" << std::endl;
-    std::cout << content << std::endl;
+    std::cout << "Saved ini file." << std::endl;
 
     return 0;
 }
